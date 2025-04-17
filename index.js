995f34c00906a7426de7515f00ff7516e29dfcbc
root <newmogly@naver.com>
2025-04-17 15:36:41 +0900

    Inial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const https = require('https');
+const express = require('express');
+const app = express();
+
+const options = {
+      key: fs.readFileSync('/etc/letsencrypt/live/appmaster.co.kr/privkey.pem'),    cert: fs.readFileSync('/etc/letsencrypt/live/appmaster.co.kr/fullchain.pem')
+}
+
+https.createServer(options, app).listen(443, () => {
+	 console.log('https Server running on port 443');
+});
+
+app.get('/', (req, res) => {
+	res.send('Hello, NCP Rocky Linux!');
+});
+
+app.listen(3000, () => {
+	console.log('Server is running on port 3000');
+});
